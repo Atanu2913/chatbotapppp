3a49a4b0d45f9f1a0cf36156eb61c97df253b902
Atanu2913 <atanu2913@gmail.com>
2022-10-06 12:17:10 +0530

    release  12

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, json, jsonify, request
 import time
+import os
 
+port = int(os.environ.get('PORT', 5000)) 
 app = Flask(__name__)
 @app.route("/bot", methods=["POST"])
 
@@ -14,5 +16,5 @@ def response():
 
 if __name__ == "__main__":
     # app.run(debug=True, host="0.0.0.0",)
-    app.run(host="0.0.0.0",)
+    app.run(host="0.0.0.0",port=port, debug=True)
     
